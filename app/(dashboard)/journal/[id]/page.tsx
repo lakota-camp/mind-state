817207b8e5cd85493b0e48fbcd440ac7f1eb5428
Lakota Camp <lakotacamp3@gmail.com>
2024-08-23 09:04:38 -0700

    added type to [id]/page.tsx

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -2,7 +2,7 @@ import Editor from '@/components/Editor';
 import { getUserByClerkId } from '@/utils/auth';
 import { prisma } from '@/utils/db';
 
-const getEntry = async (id: any) => {
+const getEntry = async (id: string) => {
   const user = await getUserByClerkId();
   const entry = await prisma.journalEntry.findUnique({
     where: {
